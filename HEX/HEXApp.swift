e75a9881392ad3218203c89ed506bae626f82e96
Joe Blau <im@joeblau.com>
2021-09-04 17:22:50 -0700

    Initial Commit

---
+++ HEX/HEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  HEXApp.swift
+//  HEX
+//
+//  Created by Joe Blau on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
