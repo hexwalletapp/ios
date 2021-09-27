dfe9d55821d4453c4b87eeb807812c489c52e112
Joe Blau <im@joeblau.com>
2021-09-27 16:08:20 -0700

    Rename Constant to k

--- Apps/HEX/Sources/Constants.swift
+++ Apps/HEX/Sources/Constants.swift
@@ -5,9 +5,9 @@ import BigInt
 import Foundation
 import SwiftUI
 
-struct Constant {
+struct k {
     static let HEARTS_UINT_SHIFT = BigUInt(72)
-    static let HEARTS_MASK = (BigUInt(1) << Constant.HEARTS_UINT_SHIFT) - BigUInt(1)
+    static let HEARTS_MASK = (BigUInt(1) << k.HEARTS_UINT_SHIFT) - BigUInt(1)
     static let SATS_UINT_SHIFT = BigUInt(56)
     static let SATS_MASK = (BigUInt(1) << SATS_UINT_SHIFT) - BigUInt(1)
     static let ONE_WEEK = BigUInt(7)
