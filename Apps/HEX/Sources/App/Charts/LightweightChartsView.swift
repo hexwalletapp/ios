f58fbf19e61615c9cffd6c834d34ac9081eb150c
Joe Blau <im@joeblau.com>
2021-10-30 19:56:44 -0700

    Add request to bitquery

--- Apps/HEX/Sources/App/Charts/LightweightChartsView.swift
+++ Apps/HEX/Sources/App/Charts/LightweightChartsView.swift
@@ -23,35 +23,37 @@ struct LightweightChartsView: UIViewRepresentable {
         let viewStore = ViewStore(store)
 
         chart.applyOptions(options: k.chartOptions())
+        let series = chart.addCandlestickSeries(options: k.candleStickSeriesOptions())
 
-        viewStore.candlesstickSeries.map { chart.removeSeries(seriesApi: $0) }
-        viewStore.lineSeries.map { chart.removeSeries(seriesApi: $0) }
-        viewStore.volumeSeries.map { chart.removeSeries(seriesApi: $0) }
+        series.setData(data: ohlcv.map { $0.barData })
+//        viewStore.candlesstickSeries.map { chart.removeSeries(seriesApi: $0) }
+//        viewStore.lineSeries.map { chart.removeSeries(seriesApi: $0) }
+//        viewStore.volumeSeries.map { chart.removeSeries(seriesApi: $0) }
 
-        switch chartType {
-        case .candlestick:
-            let candlestickSeries = chart.addCandlestickSeries(options: k.candleStickSeriesOptions())
-            candlestickSeries.setData(data: ohlcv.map { $0.barData })
+//        switch chartType {
+//        case .candlestick:
+//            let candlestickSeries = chart.addCandlestickSeries(options: k.candleStickSeriesOptions())
+//            candlestickSeries.setData(data: ohlcv.map { $0.barData })
+//
+//            viewStore.send(.binding(.set(\.$candlesstickSeries, candlestickSeries)))
+//        case .line:
+//            let lineSeries = chart.addLineSeries(options: k.lineSeriesOptoins())
+//            lineSeries.setData(data: ohlcv.map { $0.lineData })
+//
+//            viewStore.send(.binding(.set(\.$lineSeries, lineSeries)))
+//        }
 
-            viewStore.send(.binding(.set(\.$candlesstickSeries, candlestickSeries)))
-        case .line:
-            let lineSeries = chart.addLineSeries(options: k.lineSeriesOptoins())
-            lineSeries.setData(data: ohlcv.map { $0.lineData })
-
-            viewStore.send(.binding(.set(\.$lineSeries, lineSeries)))
-        }
-
-        let volumeSeries = chart.addHistogramSeries(options: k.volumeSeriesOptions())
-        volumeSeries.priceScale().applyOptions(
-            options: PriceScaleOptions(
-                scaleMargins: PriceScaleMargins(
-                    top: 0.85,
-                    bottom: 0
-                )
-            )
-        )
-
-        volumeSeries.setData(data: ohlcv.map { $0.volumeData })
-        viewStore.send(.binding(.set(\.$volumeSeries, volumeSeries)))
+//        let volumeSeries = chart.addHistogramSeries(options: k.volumeSeriesOptions())
+//        volumeSeries.priceScale().applyOptions(
+//            options: PriceScaleOptions(
+//                scaleMargins: PriceScaleMargins(
+//                    top: 0.85,
+//                    bottom: 0
+//                )
+//            )
+//        )
+//
+//        volumeSeries.setData(data: ohlcv.map { $0.volumeData })
+//        viewStore.send(.binding(.set(\.$volumeSeries, volumeSeries)))
     }
 }
